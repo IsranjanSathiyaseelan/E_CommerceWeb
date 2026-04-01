a1543aa83cb5b1f0f9b9a0d91cb1b1f0f09d1de8
Isranjan <isranjansathiyaseelan@gmail.com>
2026-04-01 18:01:53 +0530

    Adjust home page margin

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom'
 const MainBanner = () => {
   return (
     <div className='relative'>
-      <img src={assets.main_banner_bg} alt='banner' className='w-full hidden md:block' />
-      <img src={assets.main_banner_bg_sm} alt='banner' className='w-full md:hidden' />
+      <img src={assets.main_banner_bg} alt='banner' className='w-full hidden md:block rounded-md ' />
+      <img src={assets.main_banner_bg_sm} alt='banner' className='w-full md:hidden rounded-md ' />
 
       <div className='absolute inset-0 flex flex-col items-center md:items-start justify-end
-      md:justify-center pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24'>
+          md:justify-center pb-10 md:pb-0 px-4 md:pl-18 lg:pl-24'>
         <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left
-        max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15'
-        >Freshness You Can Trust, Savings You Will Love! </h1>
+          max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15'
+        >Love the Freshness, Live the Savings </h1>
      
 
       <div className='flex items-center mt-6 font-medium'>
